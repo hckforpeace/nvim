42c81611f9359105a7734b1a0f4e694c37674622
hckforpeace <pierbey@gmail.com>
2024-12-27 01:23:04 +0100

    some plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,55 @@
-require("config.lazy")
+vim.g.mapleader = " "
 vim.cmd("set number")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		-- add your plugins here
+
+		-- color plugin
+		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },   -- add your plugins here
+
+		-- FuzzyFinder
+		{
+			'nvim-telescope/telescope.nvim', tag = '0.1.8',
+			-- or                              , branch = '0.1.x',
+			dependencies = { 'nvim-lua/plenary.nvim' }
+		},
+
+		-- TreeSitter
+		{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	--  install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- Telescope set up
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
