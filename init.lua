e17cc10c8bd8cfadffe248cff1a03277112c3dc2
hckforpeace <pierbey@gmail.com>
2024-12-27 14:25:51 +0100

    treesitter

--- init.lua
+++ init.lua
@@ -10,8 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
 			{ out, "WarningMsg" },
 			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
+		}, true, {}) vim.fn.getchar()
 		os.exit(1)
 	end
 end
@@ -20,7 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
@@ -39,7 +37,10 @@ require("lazy").setup({
 		},
 
 		-- TreeSitter
-		{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+		{ 
+			"nvim-treesitter/nvim-treesitter", 
+			build = ":TSUpdate",  -- This is more reliable than the current build function
+		}
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
@@ -53,3 +54,12 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+    sync_install = false,        -- Install parsers synchronously (only applied to `ensure_installed`)
+    auto_install = false,        -- Don't auto install if parser isn't installed
+    highlight = { enable = true },
+    indent = { enable = true },  
+})
+vim.cmd.colorscheme "catppuccin"
