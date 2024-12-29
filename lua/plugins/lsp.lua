39822fe2bdd80085820a21b7faf7197b805aed96
hckforpeace <pierbey@gmail.com>
2024-12-29 01:10:49 +0100

    lsp installed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,16 +6,30 @@ return {
 		end
 	},
 	{
-		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
 		config = function()
-			require("mason-lspconfig").setup(
-				{
-					ensure_installed = { "lua_ls", "rust_analyzer" }
-					local lsconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({})
-				})
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" }
+			})
 		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			require("lspconfig").lua_ls.setup {}
+		end,
 	}
 }
